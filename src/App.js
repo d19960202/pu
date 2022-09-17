92d284d8274e2ce5553bfc1c55473cf0417b05bc
unknown <d1996020219@gmail.com>
2022-09-17 16:18:31 +0800

    wison

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import useSWR, { SWRConfig } from 'swr'
+import fetcher from "./fetcher";
+// import fetch from 'unfetch'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+function Dashboard () {
+  const { data: events } = useSWR('/youbike.json')
 }
+
+function App () {
+  return (
+    <SWRConfig 
+      value={{
+        refreshInterval: 3000,
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
+      <Dashboard />
+    </SWRConfig>
+  )
+}
+
+
+
+
+
+// export async function getStaticProps () {
+//   // `getStaticProps` is executed on the server side.
+//   const article = await getArticleFromAPI()
+//   return {
+//     props: {
+//       fallback: {
+//         '/youbike.json': article
+//       }
+//     }
+//   }
+// }
+
+// const App = () => {
+//   const {
+//       data,
+//       error,
+//   } = useSWR(`./youbike.json`, fetcher);
+//   if ( error ) return <div>failed to load</div>;
+//   if ( !data ) return <div>loading...</div>;
+//   return <div className='bike'>
+//       {
+//           data.map((item: any, index: number) => {
+//               return <ProductComponent key={ item.name } item={ item } index={ index } />
+//           })
+//       }
+//   </div>;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App () {
+//   const { data, error } = useSWR('/youbike.json', fetcher)
+
+//   if ( error ) return <div>failed to load</div>;
+//   if ( !data ) return <div>loading...</div>;
+//   // console.log(youbike);
+//     return <div>
+//       {data.map(item => <div key={item.id}>{item.name}</div>)}
+//     </div>
+//   }
+
+
+
 
 export default App;
