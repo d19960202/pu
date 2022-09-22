0f82bff835bbf991a883f5ec0dc0bcab51f02ae3
unknown <d1996020219@gmail.com>
2022-09-22 15:47:28 +0800

    wison

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import useSWR from 'swr'
+import useSWR from 'swr' ;
+import "./App.css";
 // import fetcher from "./fetcher";
 // import Item from "./item"
 // import fetch from 'unfetch'
@@ -16,22 +17,63 @@ function App ()
 
   if (!data) return <div>loading...</div>;
 
-  return <div>youbikestation {data.map((item) => {
-    return <p> {item.sno} ,{item.sna},{item.tot},{item.sbi},{item.sarea},{item.mday},{item.lat},{item.lng},{item.ar},{item.sareaen},{item.snaen},{item.aren}</p>
-  })} </div>
-    // data.map((item) =>  {
-    //   return
-    //   <>
-    //   <p> {item.sno} </p>,
-    //   <p> {item.sna} </p>,
-    //   <p> {item.sarea} </p>,
-    //   <p> {item.mday} </p>,
-    //   <p> {item.ar} </p>,
-    //   <p> {item.snaen} </p>,
-    //   <p> {item.aren} </p>,
-    //   </>
-    // })
+  return <div>youbikestation {data.map((item ) => 
+    {
+    return <table>
+      <thead>
+
+        <tr>
+          <th> sno </th>
+          <th> sna </th>
+          <th> tot </th>
+          <th> sbi </th>
+          <th> sarea </th>
+          <th> mday </th>
+          <th> lat </th>
+          <th> lng </th>
+          <th> ar </th>
+          <th> sareaen </th>
+          <th> snaen </th>
+          <th> aren </th>
+        </tr>
+
+      </thead>
+
+      <tbody>
+
+       <tr>
+          <td> {item.sno} </td>
+          <td> {item.sna} </td>
+          <td> {item.tot} </td>
+          <td> {item.sbi} </td>
+          <td> {item.sarea} </td>
+          <td> {item.mday} </td>
+          <td> {item.lat} </td>
+          <td> {item.lng} </td>
+          <td> {item.ar} </td>
+          <td> {item.sareaen} </td>
+          <td> {item.snaen} </td>
+          <td> {item.aren} </td>
+
+        </tr>
+      </tbody>
+    
+     </table> 
+    })} </div>
 }
+
+// {/* <p>  ,{},{},{},{item.},{item.mday},{item.lat},{item.lng},{item.ar},{item.sareaen},{item.snaen},{item.aren}</p> */}
+
+// function App () 
+// {
+//   const { data } = useSWR('https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json', fetcher);
+
+//   if (!data) return <div>loading...</div>;
+
+//   return <div>youbikestation {data.map((item) => {
+//     return <p> {item.sno} ,{item.sna},{item.tot},{item.sbi},{item.sarea},{item.mday},{item.lat},{item.lng},{item.ar},{item.sareaen},{item.snaen},{item.aren}</p>
+//   })} </div>
+// }
 
 
 
